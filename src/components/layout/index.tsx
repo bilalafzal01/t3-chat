2a9ecf114d306179dd27311c0bd2206ba076b863
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2022-07-27 21:13:10 +0500

    feat: initial main page

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,10 +1,13 @@
 import { PropsWithChildren } from 'react'
+import Footer from './Footer'
 import Navbar from './Navbar'
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <>
       <Navbar />
+      {children}
+      <Footer />
     </>
   )
 }
